4c07776a1d828a2202abd0e67bc8c3e00ec88f09
Gab0 <gabrielsilveira46@gmail.com>
2026-01-05 21:31:54 +0000

    05/01/2026
    1. Changed the JavaScript file to the superior repository and make it as a general file, where all of the basic functions will be created.
    2. Main dice rolls functions created:
    2.1 - rollStatusDice() = function to define the status points of the character and already adding the bonus for each roll;
    2.2 - rollD10() = basic roll for the general tests of the game, that will be made by rolling a d10;
    2.3 - rollD6() = how much damage the attacks will deal, with the bonus of the status.

--- character_page/character_page_javascript.js
+++
@@ -1,8 +0,0 @@
-// Dice number generator:
-function rollStatusDice () {
-    let diceResult = Math.floor(Math.random() * 6);
-    console.log(diceResult + 1);
-}
-
-rollStatusDice();
-
---
+++ the_game.js
@@ -0,0 +1,41 @@
+// Dice number generator and skill points
+function rollStatusDice () {
+    let diceResult = Math.floor(Math.random() * 6);
+    console.log(diceResult + 1);
+    // Status bonus
+    if (diceResult === 1) {
+        return statBonus = 0;
+    } else if (diceResult >= 2 && diceResult <= 4) {
+        return statBonus = 1;
+    } else if (diceResult >= 5) {
+        return statBonus = 2;
+    }
+    let totalStatus = diceResult + statBonus;
+    console.log(totalStatus);
+}
+
+rollStatusDice();
+
+// Dice result evaluator
+//D10 - tests
+    function rollD10 () {
+        let d10Result = Math.floor(Math.random() * 10) + 1;
+        if (d10Result === 1) {
+            console.log(`${d10Result}! It's a critical failure!`);
+        } else if (d10Result === 10) {
+            console.log(`${d10Result}! It's a critical success!`);
+        } else if (d10Result >= 2 && d10Result <= 5) {
+            console.log(`${d10Result}! Failure!`);
+        } else if (d10Result >= 6 && d10Result <= 9) {
+            console.log(`${d10Result}! Success!`);
+        }
+    }
+
+    rollD10();
+//D6 - damage
+    function rollD6 () {
+        let rollD6 = Math.floor(Math.random() * 6) + 1;
+        console.log(`Attack = ${rollD6 + statBonus}`);
+    }
+
+    rollD6();
